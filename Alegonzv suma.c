88e1cdd50b5204da6bd256ba43afdb4ae371955a
Prueba1 <alegonza212010@hotmail.com>
2016-08-25 08:20:34 -0500

    Update Alegonzv suma.c

--- Alegonzv suma.c
+++ Alegonzv suma.c
@@ -8,3 +8,23 @@ int main(){
     printf("La suma de %d mas %d es %d",a,b,a+b);
     return 0;
 }
+
+
+
+//problema 2 clase
+
+ main(){
+     int fahr , celsius;
+     int lower, upper,step;
+
+     lower=0;
+     upper=300;
+     step=20;
+
+     fahr=lower;
+     while (fahr <=upper){
+        celsius=5*(fahr-32)/9;
+        printf("%d\t%d\n",fahr,celsius);
+        fahr=fahr +step;
+    }
+
